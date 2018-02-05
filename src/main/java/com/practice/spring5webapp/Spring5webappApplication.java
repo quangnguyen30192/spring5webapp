6cdb5a1af47d65be98b0cc9e79f0b412010fd43f
Quang Nguyen <quangnbnse90114@gmail.com>
2018-02-05 20:06:55 +0700

    Add custom banner and addtional beans xml configuration

--- src/main/java/com/practice/spring5webapp/Spring5webappApplication.java
+++ src/main/java/com/practice/spring5webapp/Spring5webappApplication.java
@@ -2,11 +2,13 @@ package com.practice.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:app-config.xml")
 public class Spring5webappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webappApplication.class, args);
+    }
 }
